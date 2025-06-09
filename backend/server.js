1c74180ee3b8070f8f39e7614c8c5d7d998f4f84
adhmara-prani <parth.saxena1021@gmail.com>
2025-06-09 16:01:52 +0530

    Deployment (finally)

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectToDB from "./db/connectToMongoDb.js";
 import cookieParser from "cookie-parser";
+import path from 'path';
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -11,12 +12,20 @@ import { app, server} from "./socket/socket.js";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser()); // to use middleware in between the routes as a middleman in communication between messages and the user authorization
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 
 server.listen(PORT, () => {
     connectToDB();
