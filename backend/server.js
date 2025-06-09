d207a12e430c97f92892419bae986997f2a74f6b
adhmara-prani <parth.saxena1021@gmail.com>
2025-06-09 14:11:53 +0530

    Added real-time online/offline status

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,11 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
-import userRoutes from "./routes/user.routes.js"
+import userRoutes from "./routes/user.routes.js";
+import { app, server} from "./socket/socket.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
-
-const app = express();
 
 app.use(express.json());
 app.use(cookieParser()); // to use middleware in between the routes as a middleman in communication between messages and the user authorization
@@ -19,7 +18,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectToDB();
     console.log(`Server is running on ${PORT}`);
 });
--- frontend/src/components/Messages/MessageContainer.jsx
+++ frontend/src/components/Messages/MessageContainer.jsx
@@ -3,6 +3,7 @@ import Messages from './Messages.jsx'
 import MessageInput from './MessageInput.jsx'
 import { TiMessages } from "react-icons/ti";
 import useConversation from '../../zustand/useConversation.js';
+import { useAuthContext } from '../../context/AuthContext.jsx';
 
 const MessageContainer = () => {
   const {selectedConversation, setSelectedConversation} = useConversation();
@@ -32,16 +33,17 @@ const MessageContainer = () => {
   )
 }
 
+export default MessageContainer
+
 const NoChatSelected = () => {
+  const {authUser} = useAuthContext();
   return (
     <div className='flex items-center justify-center w-full h-full'>
       <div className='px-4 text-center sm:text-lg md:text-2xl text-gray-800 flex font-semibold flex-col items-center gap-2'>
-        <p>Welcome Velma</p>
+        <p>Welcome {authUser.fullName}</p>
         <p>Select a chat to start messaging</p>
         <TiMessages className='text-3xl md:text-6xl flex items-center' />
       </div>
     </div>
   )
 }
-
-export default MessageContainer
